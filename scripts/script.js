9b75214e1742e1330cba90a2d80d3b9853d09379
Daniel Lima <danielmlimag@gmail.com>
2021-11-17 19:39:39 -0300

    feat: add footer and some incomplete changes (emergency commit)

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const Gameboard = () => {
+	let gameboard = [
+		['x', 'o', 'x'],
+		['x', 'o', 'x'],
+		['x', 'o', 'x'],
+	];
+	return 0;
+};
+
+const Player = () => {
+	return 0;
+};
+
+let player1 = Player();
+let player2 = Player();
+
+const gameFlow = () => {
+	return 0;
+};
