21842ac54a491a951643f945ea36861dae8c5573
Daniel Lima <danielmlimag@gmail.com>
2022-07-04 17:50:43 -0300

    feat: implement create and render gameboard methods

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,80 @@
 'use strict';
 
+let num = 0;
+
+function convertDecToTer() {
+	let quotient = num / 3;
+	let remainder = num % 3;
+	if (quotient == 0) {
+		return '';
+	} else {
+		return convertDecToTer(quotient + remainder);
+	}
+}
+
 const Gameboard = (() => {
 	let gameboard = [
 		['x', 'o', 'x'],
 		['x', 'o', 'x'],
 		['x', 'o', 'x'],
 	];
+
+	// maybe put render method in this class
+
 	return { gameboard };
 })();
 
 const Player = () => {
-	const click = () => {
-
-	};
-	return {click};
+	return {};
 };
 
 let topPlayer = Player();
 let bottomPlayer = Player();
 
-const displayController = (() => {
-	const renderGameboard = () => {};
-	return { renderGameboard };
+const gameFlow = (() => {
+	// maybe put createGameboardDiv and renderGameboard methods as create and render methods on Gameboard class
+
+	const createGameboardDiv = () => {
+		let gameboardDiv = document.querySelector('#gameboard');
+
+		for (let i = 0; i < 9; i++) {
+			let spotDiv = document.createElement('div');
+			spotDiv.className = 'spot';
+			console.log(convertDecToTer(0));
+			spotDiv.id = `${0}${0}`;
+			gameboardDiv.append(spotDiv);
+		}
+	};
+
+	const renderGameboard = () => {
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+				let square = document.getElementById(`${i}${j}`);
+				let symbol = document.createElement('img');
+				symbol.src = `./assets/${Gameboard.gameboard[i][j]}.png`;
+				symbol.className = `${Gameboard.gameboard[i][j]}-image`;
+				square.append(symbol);
+			}
+		}
+	};
+
+	// const = checkEndOfRound = () => {}
+	// const = checkGameOver = () => {}
+
+	return { renderGameboard, createGameboardDiv };
 })();
 
-let gameboardDiv = document.querySelector("#gameboard");
+gameFlow.createGameboardDiv();
+gameFlow.renderGameboard();
 
-for (let i = 0; i < 9; i++) {
-	let spotDiv = document.createElement("div");
-	spotDiv.className = "spot";
-	gameboardDiv.append(spotDiv);
-}
+// let first_space = document.querySelector('.spot:nth-child(1)');
+// let xImg = document.createElement('img');
+// xImg.src = './assets/x.png';
+// xImg.id = 'x-image';
+// first_space.append(xImg);
 
-let teste = document.querySelector(".spot:nth-child(1)");
-teste.style.backgroundColor = "black";
+// let second_space = document.querySelector('.spot:nth-child(2)');
+// let oImg = document.createElement('img');
+// oImg.src = './assets/o.png';
+// oImg.id = 'o-image';
+// second_space.append(oImg);
